c6356f3e2e95d81ad846e67a44706f056f29bc11
MissLitaa <anahug49@gmail.com>
2022-02-08 14:49:13 +0100

    6
    6

--- Assets/Scripts/spawnManager.cs
+++ Assets/Scripts/spawnManager.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class spawnManager : MonoBehaviour
 {
-    public GameObject girlScout;
+    public GameObject[] girlScout;
+    public GameObject[] snowProyectile;
+    public GameObject player;
+
+    private Vector3 girlScoutSpawnPos;
+    private Vector3 snowProyectilePos;
+
+    public int girlScoutIndex;
+    public int snowProyectileIndex;
+
+    
     void Start()
     {
-        transform.position = new Vector3(-11, 1, 0);
-        InvokeRepeating("SpawnGirlScout", time: 2, repeatRate: 1.5f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
+        InvokeRepeating("SpawnGirlScout", time: 2, repeatRate: 1.5f);
     }
 
     public void SpawnGirlScout()
     {
+        girlScoutSpawnPos = new Vector3(-11, 1, 0);
 
+        girlScoutIndex = girlScout.Length;
+        Instantiate(girlScout[girlScoutIndex], girlScoutSpawnPos, girlScout[girlScoutIndex].transform.rotation);
+    }
+
+    public void SpawnSnowProyectile()
+    {
+        snowProyectilePos = player.transform.position ++ ;
+
+        girlScoutIndex = girlScout.Length;
+        Instantiate(snowProyectile, transform.position, snowProyectile.transform.rotation);
     }
 }
