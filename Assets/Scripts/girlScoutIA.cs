843efafa6af32a1a7b91d58abf67aed340307ede
MissLitaa <anahug49@gmail.com>
2022-02-14 21:21:15 +0100

    9
    9

--- Assets/Scripts/girlScoutIA.cs
+++ Assets/Scripts/girlScoutIA.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class girlScoutIA : MonoBehaviour
 {
     public GameObject player;
     private NavMeshAgent navMesh;
+    public ParticleSystem snowBlowPS;
+    public ParticleSystem snowWalkPS;
+
+    private bool isPlaying = false;
    
     void Start()
     {
         navMesh = GetComponent<NavMeshAgent>();
+        snowBlowPS = GetComponent<ParticleSystem>();
+        snowWalkPS = GetComponent<ParticleSystem>();
     }
 
     
@@ -26,12 +33,16 @@ public class girlScoutIA : MonoBehaviour
             Time.timeScale = 0;
             //Game over goes here.
         }
+       
+
+
 
         if (collision.gameObject.tag == "SnowProyectile")
         {
-            //Particles go here.
+            snowWalkPS.Stop();
+            snowBlowPS.Play();
             //Animation girl scout death here.
-            //Destroy game object here.
+            Destroy(gameObject);
             //Generate coins. Call to coin script here.
         }
     }
