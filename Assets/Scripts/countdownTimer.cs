bd1e5a86adcb479b98dc71de2c96a586afe27d62
MissLitaa <anahug49@gmail.com>
2022-02-24 13:56:07 +0100

    17
    17

--- Assets/Scripts/countdownTimer.cs
+++ Assets/Scripts/countdownTimer.cs
@@ -1,18 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class countdownTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [HideInInspector] public float currentTime = 0f;
+    [HideInInspector] public float startingTime = 30f;
+    [HideInInspector] public TextMeshProUGUI timeDisplay;
+    [HideInInspector] public bool timerIsRunning;
+    [HideInInspector] public bool restartTimer;
+
+    public ParticleSystem confettiParticles;
+    public AudioSource confettiAudio;
+
+    public GameObject shopGEO;
+    public GameObject player;
+    [HideInInspector] public bool isShopOpen;
+    [HideInInspector] public bool isInShop;
+    [HideInInspector] public Vector3 shopPosition;
+    [HideInInspector] public Vector3 playerPosition;
+    [HideInInspector] public int playerIsInRange = 2;
+
+    [HideInInspector] public int methodIsRunning = 0;
+
     void Start()
+    {
+        currentTime = startingTime;
+        timerIsRunning = true;
+        shopPosition = shopGEO.transform.position;
+    }
+
+    // Update is called once per frame
+    IEnumerator Update()
+    {
+        playerPosition = player.transform.position;
+        currentTime -= 1 * Time.deltaTime;
+        timeDisplay.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            timerIsRunning = false;
+            //stop enemy spawn here
+
+            if (methodIsRunning == 0 && timerIsRunning == false)
+            {
+                methodIsRunning++;
+                openShop();
+            }
+
+            //particles confetti here
+
+            //confetti sound here
+
+
+        }
+
+        while (timerIsRunning)
+        {
+            isShopOpen = false;
+            isInShop = false;
+            yield return null;
+        }
+    }
+
+    public void FixedUpdate()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void openShop()
     {
-        
+        isShopOpen = true;
+        float distance = Vector3.Distance(playerPosition, shopPosition);
+
+        if (distance < playerIsInRange)
+        {
+            if (Input.GetButtonDown("E"))
+            {
+                isInShop = true;
+                //call shop system here. (scenemode.additive, el canvas de la hud de la tienda)
+                //update inventory if necessary.
+            }
+
+            else if (&& isInShop = true)
+            {
+                //close shop system first
+                isInShop = false;
+                isShopOpen = false;
+            }
+        }
     }
 }
+
